eb663eda960d8227bd66e312157f3e5c2424ce9f
alekseik1 <1alekseik1@gmail.com>
2019-11-06 16:08:14 +0300

    fix: syntax in comparison feat: better warning system

--- hw1/calculator.sh
+++ hw1/calculator.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 if [[ $# -ne 3 ]]; then
     echo "Usage: <number 1> <number 2> <operation>"
-    echo "where <operation> can be '+', '-', '*', '/' (without quotes)"
+    echo "where <operation> can be '+', '-', 'x' '/' (without quotes)"
     exit 1
 fi
 
+operation=$3
+
+if [[ $3 = 'x' ]]; then
+    operation=\*
+fi
+
 if hash bc 2>/dev/null; then
-    bc -l <<< "$1$3$2" | xargs echo
+    bc -l <<< "$1$operation$2" | xargs echo
     exit 0
 else
-    if [[ $3 -eq '/' ]]; then
-       echo "No 'bc' utility is found. Division will be integer"
+    if [[ $3 = '/' ]]; then
+       echo "WARNING: no 'bc' utility is found. Division will be integer"
     fi
-    let result=$1$3$2
+    let result=$1$operation$2
     echo $result
     exit 0
 fi

5281cb83ba7d084b14288676216b21919d4e786d
alekseik1 <1alekseik1@gmail.com>
2019-11-16 14:18:41 +0300

    pi: remove unnecessary python scripts

--- hw1/pi/Pi.py
+++ hw1/pi/Pi.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import numpy as np
+import math
 import sys
 import time
 
@@ -7,28 +8,26 @@ comm = MPI.COMM_WORLD
 myrank = comm.Get_rank()
 nproc = comm.Get_size()
 PI25DT = 3.141592653589793238462643
+done = 0
 n, p = 0, 0
 if myrank == 0:
     n = int(sys.argv[1])
     p = int(sys.argv[2])
-
 start = time.time()
-
 n = comm.bcast(n, root = 0)
-h = 1.0 / float(n)
-sum = 0.0
+h = np.array((1.0 / float(n)), dtype='f')
+sum = np.array(0.0, dtype = 'f')
 i = myrank + 1
 while i <= n:
-	x = h * (float(i) - 0.5)
-	sum += 4.0 / (1.0 + x*x)
-	i += nproc
+    x = h * (float(i) - 0.5)
+    sum += 4.0 / (1.0 + x*x)
+    i += nproc
 mypi = h * sum
-tot_sum = 0.0
-tot_sum = comm.reduce(mypi, op=MPI.SUM, root=0)
+tot_sum = np.array(0.0, dtype='f')
+comm.Reduce([mypi, 1, MPI.FLOAT], [tot_sum, 1, MPI.FLOAT], op=MPI.SUM, root=0)
 end = time.time()
 if myrank == 0:
     print(f'{p};{n};{end-start}')
-    # print("pi is approximately {0}\n".format(tot_sum))
-    # print("error is {0}\n".format(abs(PI25DT-tot_sum)))
-    # print(myrank, end - start)
+	# print("pi is approximately {0}\n".format(tot_sum))
+	# print("error is {0}\n".format(abs(PI25DT-tot_sum)))
 
--- hw1/pi/run.sh
+++ hw1/pi/run.sh
@@ -2,6 +2,6 @@ for p in 1 2 3 4 5 6 7
 do 
     for n in 1000 10000 100000 1000000 
     do
-        sbatch -J alek1 -p RT -N 1 -o "n_$n;p_$p.out" --ntasks-per-node=$p --wrap="mpirun -np ${p} python Pi2_.py ${n} ${p}" --exclusive
+        sbatch -J alek1 -p RT -N 1 -o "n_$n;p_$p.out" --ntasks-per-node=$p --wrap="mpirun -np ${p} python Pi.py ${n} ${p}" --exclusive
     done
 done

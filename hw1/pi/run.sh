6e9853868ee5c826f50945713f0d8ebe401d4f3a
alekseik1 <1alekseik1@gmail.com>
2019-11-17 21:40:28 +0300

    pi: more data dots pi: 'Time from p' graph for c)

--- hw1/pi/run.sh
+++ hw1/pi/run.sh
@@ -1,6 +1,6 @@
-for p in 1 2 3 4 5 6 7 
+for p in $(seq 1 16)
 do 
-    for n in 1000 10000 100000 1000000 
+    for n in 1000 5000 10000 50000 100000 500000 1000000 
     do
         sbatch -J alek1 -p RT -N 1 -o "n_$n;p_$p.out" --ntasks-per-node=$p --wrap="mpirun -np ${p} python Pi.py ${n} ${p}" --exclusive
     done
---
+++ hw1/pi/run2.sh
@@ -0,0 +1,7 @@
+for p in $(seq 1 16)
+do 
+    for n in 1000 5000 10000 50000 100000 500000 1000000 
+    do
+        sbatch -J alek1 -p RT -N 1 -o "n_$n;p_$p.out" --ntasks-per-node=$p --wrap="mpirun -np ${p} python Pi2.py ${n} ${p}" --exclusive
+    done
+done

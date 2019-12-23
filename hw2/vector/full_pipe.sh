3d4833cfd50d355bd466d2011e9575de078f4a60
alekseik1 <1alekseik1@gmail.com>
2019-12-23 22:36:34 +0300

    feat: more logging

--- hw2/vector/full_pipe.sh
+++ hw2/vector/full_pipe.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 for i in $(seq 1 10)
 do
-    bash run.sh && bash wait_results.sh && bash parse_data.sh
+    bash run.sh
+    echo Waiting for results...
+    echo Running $i of 10
+    bash wait_results.sh
+    echo Results are ready, combining
+    echo Running $i of 10
+    bash parse_data.sh
+    echo Done combining!
 done

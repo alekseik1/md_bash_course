d4c60ea6dce937f6d336a7265e2c9e19b503885f
alekseik1 <1alekseik1@gmail.com>
2019-12-23 22:35:07 +0300

    fix: async send fix: proper time measurement fix: split via numpy built-in method

--- hw2/vector/dot_my.py
+++ hw2/vector/dot_my.py
@@ -4,26 +4,14 @@ import time
 import sys
 
 def send_parts(x, y, comm, node):
-    comm.send(x, dest=node, tag=0)
-    comm.send(y, dest=node, tag=1)
+    req1 = comm.isend(x, dest=node, tag=0)
+    req2 = comm.isend(y, dest=node, tag=1)
+    return [req1, req2]
 
 def sum_send(comm):
     own_x = comm.recv(source=0, tag=0)
     own_y = comm.recv(source=0, tag=1)
-    comm.send(np.dot(own_x, own_y), dest=0, tag=5)
-
-
-def split_vectors(x, node, nproc):
-    base_length = len(x)//nproc
-    lengths = [base_length]*nproc
-    i = -1
-    while sum(lengths) < len(x):
-        lengths[i] += 1
-        i -= 1
-    for k in range(nproc):
-        start = sum(lengths[:k])
-        stop = sum(lengths[:k+1])
-        yield x[start:stop], y[start:stop]
+    comm.send(sum([own_x[i]*own_y[i] for i in range(len(own_x))]), dest=0, tag=5)
 
 
 if __name__ == '__main__':
@@ -32,15 +20,17 @@ if __name__ == '__main__':
     myrank = comm.Get_rank()
     nproc = comm.Get_size()
     n, x, y = 0, np.array([]), np.array([])
-    start_time = time.time()
     if myrank == 0:
         n = int(sys.argv[1])
-        x, y = np.random.rand(n), np.random.rand(n)
-        i = 0
-        for x_part, y_part in split_vectors(x, y, nproc):
-            send_parts(x_part, y_part, comm, i)
-            i += 1
-        total_sum = sum([comm.recv(source=i, tag=5) for i in range(nproc)])
+        #x, y = np.random.rand(n), np.random.rand(n)
+        x, y = np.empty(n), np.empty(n)
+        start_time = time.time()
+        reqs = []
+        for i, arr in enumerate(np.array_split(np.array([x, y]), nproc-1, axis=1)):
+            reqs.extend(send_parts(arr[0, :], arr[1, :], comm, i+1))
+        [req.wait() for req in reqs]
+        total_sum = sum([comm.recv(source=i, tag=5) for i in range(1, nproc)])
+        print(f'{n};{nproc};{time.time() - start_time}')
     else:
         sum_send(comm)
 
